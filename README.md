e58fda92ecf831507d9769f0008e49f177d5d544
csh6357 <csh6357@sjinc.co.kr>
2024-03-04 16:17:29 +0900

    ㅅ

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sns_project
+
+### Main Page
+
+![main](https://github.com/csh29/sns_project/assets/86338199/4132c2e4-4178-4287-ab01-21177b39e546)
--- back/passport/kakao.js
+++ back/passport/kakao.js
@@ -13,29 +13,37 @@ module.exports = () => {
         },
         async(accessToken, refreshToken, profile, done) => {        
             try{
-                const exUser = await User.findOne({
-                    where : {kakaoId: profile.id}
-                });
                 const profileImage = profile._json.properties.profile_image
-                if(exUser){
-                    await User.update({
-                        profileImageUrl: profileImage
-                    },{
-                        where: {kakaoId: profile.id}
-                    })
-                    const data = { user:exUser ,  accessToken}
-                    done(null, data)
-                } else {
+                const exUser = await User.findOne({
+                    where : {email: profile.displayName+"_kakao"}
+                });
+
+                if(!exUser) {
+                    const hashedPassword = await bcrypt.hash(profile.id+'', 12);
+
                     const newUser = await User.create({                    
                         email: profile.displayName+"_kakao",
                         nickname: profile.displayName,
-                        userId: profile.id,
-                        kakaoId: profile.id,
+                        kakaoId: hashedPassword,
                         profileImageUrl: profileImage
                     });
                     const data = { user:newUser ,  accessToken}
                     done(null, data)
+                } else {
+                    const result = await bcrypt.compare(profile.id+'', exUser.kakaoId);
+    
+                    if(result) {
+                        await User.update({
+                            profileImageUrl: profileImage
+                        },{
+                            where: {kakaoId: exUser.kakaoId}
+                        })
+                        const data = { user:exUser ,  accessToken}
+                        done(null, data)
+                    } 
                 }
+
+                
             }catch(error){
                 console.log(error)
             }
